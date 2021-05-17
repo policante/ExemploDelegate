d915e33fe9a0f7ddd91239fbbb553f409ab1a8d0
Rodrigo Martins <rodrigo.martins@jamesdelivery.com.br>
2021-05-17 20:17:46 -0300

    Initial Commit

---
+++ ExemploDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExemploDelegate
+//
+//  Created by Rodrigo Martins on 17/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
